fdd731d7dc005f01acca9bbf36c79fb3ba7fbf67
Paulo César <pcgms7@hotmail.com>
2023-03-10 00:01:56 -0300

    finalizando prejeto

--- src/styles.ts
+++ src/styles.ts
@@ -1,8 +1,7 @@
-:root {
-  --cor-principal: #a7727d;
-  --cor-secundaria: #f9f5e7;
-}
+import styled, {createGlobalStyle} from "styled-components";
 
+
+const EstiloGlobal = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
@@ -26,3 +25,7 @@ body {
     max-width: 80%;
   }
 }
+`
+
+export default EstiloGlobal
+
